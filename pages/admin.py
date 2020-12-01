dd81f23309b07184c16d15a222eea17f9648918b
Jeremy Artiflas <Artiflas@gmail.com>
2020-12-01 06:09:43 +0100

    Completed Teams Model

--- pages/admin.py
+++ pages/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import Team
+from django.utils.html import format_html
+
 
 # Register your models here.
+
+class TeamAdmin(admin.ModelAdmin):
+    def thumbnail(self, object):
+        return format_html('<img src="{}" width="40" style="border-radius: 50px"/>'.format(object.photo.url))
+
+    thumbnail.short_description = 'Bild'
+
+    list_display = ('id', 'thumbnail', 'vorname', 'familienname',
+                    'beruf', 'create_date')
+
+    list_display_links = ('id', 'thumbnail', 'vorname', 'familienname')
+    search_fields = ('vorname', 'familienname', 'beruf', 'create_date')
+    list_filter = ('beruf',)
+
+
+admin.site.register(Team, TeamAdmin)
--- pages/models.py
+++ pages/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Team(models.Model):
+    vorname = models.CharField(max_length=255)
+    familienname = models.CharField(max_length=255)
+    beruf = models.CharField(max_length=255)
+    photo = models.ImageField(upload_to='photos/%Y/%m/%d/')
+    facebook_link = models.URLField(max_length=100)
+    twitter_link = models.URLField(max_length=100)
+    google_plus_link = models.URLField(max_length=100)
+    create_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.vorname
